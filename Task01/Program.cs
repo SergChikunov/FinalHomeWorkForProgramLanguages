42996eaa8fd8b9d75680416e77df86aebf6180f0
Sergey Chikunov <consull@outlook.com>
2024-05-25 19:09:05 +0500

    Финальное ДЗ по Csharp. Задание 1. Вывод значений между натуральными N и M с использованием рекурсии

---
+++ Task01/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ДЗ семинар 7. Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+ */
+
+void ShowNumbersFromNtoM(int valueN, int valueM) //рекурсивная функция
+{
+    int tmpValue = valueN - valueM; //определяем разность двух чисел, для понимания в каком направлении двигаться - на возрастание или убывание
+    if (tmpValue > 0)
+    {
+        ShowNumbersFromNtoM(valueN, ++valueM); // если N большее, тогда значение M последовательно увеличиваем в рекурсивном вызове
+        Console.Write($"{valueM}\t");
+    }
+    else if (tmpValue < 0) //если N меньшее, тогда значение M последовательно уменьшаем в рекурсивном вызове
+    {
+        ShowNumbersFromNtoM(valueN, --valueM);
+        Console.Write($"{valueM}\t");
+    }
+    else return; //а это условие выхода, собственно, если оба условия выше не соблюдаются, то выходим из метода
+}
+
+void CheckCorrectInput(string input) //впечатлило, что с Try.Parse выходит кратко и эффективно
+{
+    if (int.TryParse(input, out int _) == false) //проверяем ввод на числовое значение, итог проверки присваиваем временной переменной, завершаем код, еслии введено не число
+    {
+        Console.WriteLine("Введено не числовое значение, программа завершает работу!");
+        Environment.Exit(0); //это я подсмотрел в сети, по кодам не разбирался, но понял что 0 - успешное завершение, поэтому указал его
+    }
+}
+
+int GetNumN()
+{
+    Console.WriteLine("Введите натуральное N, это должно быть число! Завершите ввод нажатием Enter...");
+    string? inputN = Console.ReadLine(); //знак впороса подавляет вывод предупреждений о пустом значении, подсмотрел в сети
+    CheckCorrectInput(inputN);
+    int numberN = int.Parse(inputN);
+    return numberN;
+}
+
+int GetNumM()
+{
+    Console.WriteLine("Введите натуральное M, это должно быть число! Завершите ввод нажатием Enter...");
+    string? inputM = Console.ReadLine();
+    CheckCorrectInput(inputM);
+    int numberM = int.Parse(inputM);
+    return numberM;
+}
+
+int numberN = GetNumN();
+int numberM = GetNumM();
+
+ShowNumbersFromNtoM(numberN, numberM);
+Console.Write(numberM); //по условию задачи необходимо вывести весь диапазон между N и M, эта строка добавляет значение M в конец строки
